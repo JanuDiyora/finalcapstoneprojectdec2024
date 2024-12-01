f61efdf3c6afc17e0c0d2af326f52054d457b37d
janvi <gmail>
2024-12-01 16:42:55 -0500

    dec 1

--- src/components/RealEstate.js
+++ src/components/RealEstate.js
@@ -10,12 +10,12 @@ function RealEstate() {
       <h1><center>Real Estate and Infrastructure</center></h1>
       <div className="industry-grid-container">
         <div className="grid">
-          <IndustryCard title="Smart Home" icon={<MdHome size={50} className="multicolor-icon" />} link="/RPMCalculator" />
-          <IndustryCard title="Smart Building" icon={<FaBuilding size={50} className="icon-color-1" />} link="/RPMCalculator" />
-          <IndustryCard title="Smart Office" icon={<MdBusiness size={50} className="icon-color-2" />} link="/RPMCalculator" />
-          <IndustryCard title="Smart Highway" icon={<FaRoad size={50} className="multicolor-icon" />} link="/RPMCalculator" />
-          <IndustryCard title="Smart Parking" icon={<MdLocalParking size={50} className="icon-color-1" />} link="/RPMCalculator" />
-          <IndustryCard title="Smart Energy Management" icon={<MdPower size={50} className="icon-color-2" />} link="/RPMCalculator" />
+          <IndustryCard title="Smart Home" icon={<MdHome size={50} className="multicolor-icon" />} link="http://localhost:8012/smartHome" />
+          <IndustryCard title="Smart Building" icon={<FaBuilding size={50} className="icon-color-1" />} link="http://localhost:8012/smartBuilding" />
+          <IndustryCard title="Smart Office" icon={<MdBusiness size={50} className="icon-color-2" />} link="http://localhost:8012/smartOffice" />
+          <IndustryCard title="Smart Highway" icon={<FaRoad size={50} className="multicolor-icon" />} link="http://localhost:8080/smartHighway" />
+          <IndustryCard title="Smart Parking" icon={<MdLocalParking size={50} className="icon-color-1" />} link="http://localhost:8080/smartHome" />
+          <IndustryCard title="Smart Energy Management" icon={<MdPower size={50} className="icon-color-2" />} link="http://localhost:8080/smartHome" />
         </div>
       </div>
     </div>
